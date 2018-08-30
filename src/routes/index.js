76f72e00233b604e3e664b085205a45dc6a92141
jeniferatu <jeniferatu12@gmail.com>
2018-08-30 17:46:44 +0700

    semua jd data

--- src/routes/index.js
+++ src/routes/index.js
@@ -10,54 +10,83 @@ router.get('/', async(req, res) => {
 });
 
 router.get('/alik',async(req, res) => {
-    const row = await db.query('select * from user'); //utk input query manual
+    const row = await db.query('select * from daftarnama'); //utk input query manual
     res.send( row);
 });
 
-router.post('/createUser', async(req, res) => {
-    const {id, nama, nama_keuangan, nama_penjadwalan, nama_bau, gelar, nik, rekening_bank, nama_rek_dki} = req.body;//input dr browser
+router.get('/showData', async(req,res) => { //utk menampilkan data saja dg request dan response
+    const result = await db.query('select * from daftarnama');// perintah querynya untk mencari tabel dari tabel nana
+    res.send(result); // response untuk mengesend result
+});
+
+router.get('/detailnama1/:id',async(req,res) => {
+    const rows = await db.query('SELECT Nama FROM daftarnama WHERE Nama= '+  req.params.id);
+    res.send(rows);
+});
+
+router.get('/detailNama/:nama',async(req,res) => {
+    const nama = req.params.nama;
+    const rows = await db.query(`SELECT Nama FROM daftarnama WHERE Nama = ${nama}`);
+    res.send(rows);
+});
+
+router.get('/detailnama1/:id',async(req,res) => {
+    const id = req.params.id;
+    const rows = await db.query(`SELECT * FROM daftarnama where like '{id}%' `);
+    res.send(rows);
+});
+
+router.get('/showData/:id', async(req,res) => { //utk menampilkan data saja dg request dan response
+    const result = await db.query('select * from daftarnama where Id=' + req.params.id);// perintah querynya untk mencari tabel dari tabel nana
+    res.send(result); // response untuk mengesend result
+});
+
+router.get('/detailnama3/:id', async(req, res) => {
+    const id = req.params.id; //manggil no (no itu param) bentuknya slash / '%${query}%'
+    const result = await db.query(`select Nama from daftarnama where Id=${id}`); 
+    // isi dr petikan isi query spy bisa masukin variabel
+    res.send(result);
+})
+
+router.post('/createData', async(req, res) => {
+    const { id, Nama, Nama_Keuangan, Nama_Penjadwalan, Nama_BAU, Gelar, NIK, Id_Rekening, Nama_Keu} = req.body;//input dr browser
     const tableName = 'daftarnama';
     const tableValue = {
         ID : id,
+        Nama : Nama,
+        Nama_Keuangan : Nama_Keuangan,
+        Nama_Penjadwalan : Nama_Penjadwalan,
+        Nama_BAU : Nama_BAU,
+        Gelar : Gelar,
+        NIK : NIK,
+        Id_Rekening : Id_Rekening,
+        Nama_Keu : Nama_Keu
+    }
+    const result = await db.insertRow(tableName, tableValue, res);
+    res.sendStatus(200);
+});
+
+
+router.post('/updateData/:id', async(req, res) => {
+    const {nama, nama_keuangan, nama_penjadwalan, nama_bau, gelar, nik, id_rekening, nama_keu} = req.body;
+    const id = req.params.id;
+    const tableName = 'daftarnama';
+    const condition = { //utk kayak where kyk kondisi
+        Id:id
+    }
+    const tableValue = {
         Nama : nama,
         Nama_Keuangan : nama_keuangan,
         Nama_Penjadwalan : nama_penjadwalan,
         Nama_BAU : nama_bau,
         Gelar : gelar,
         NIK : nik,
-        Rekening_Bank : rekening_bank,
-        Nama_Rek_DKI : nama_rek_dki
-    }
-    const result = await db.insertRow(tableName, tableValue, res);
-    res.sendStatus(200);
-});
-
-router.get('/showdata', async(req,res) => { //utk menampilkan data saja dg request dan response
-    const result = await db.query('select * from daftarnama');// perintah querynya untk mencari tabel dari tabel nana
-    res.send(result); // response untuk mengesend result
-});
-
-
-router.post('/updateUser/:no', async(req, res) => {
-    const {nama,rekening} = req.body;
-    const no = req.params.no;
-    const tableName = 'user';
-    const condition = { //utk kayak where kyk kondisi
-        no:no
-    }
-    const tableValue = {
-        nama: nama, 
-        rekening: rekening
+        Id_Rekening :id_rekening,
+        Nama_Keu : nama_keu
     }
     const result = await db.updateRow(tableName, tableValue, condition, res); 
     res.sendStatus(200);
 })
 
-router.get('/detailnama/:no', async(req, res) => {
-    const no = req.params.no; //manggil no (no itu param) bentuknya slash /
-    const result = await db.query(`select Nama from daftarnama where Id=${no}`); 
-    // isi dr petikan isi query spy bisa masukin variabel
-    res.send(result);
-})
 
 export default router;
